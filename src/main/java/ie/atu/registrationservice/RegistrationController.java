3022ea01df0bae0b9a0e32481ca14f995f811997
Patryk Milkiewicz <g00397265@atu.ie>
2023-10-15 13:29:06 +0100

    Created RegistrationDetails Class with a post mapping

---
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -0,0 +1,14 @@
+package ie.atu.registrationservice;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    @PostMapping("/confirm")
+    public String confDetails(@RequestBody UserDetails userDetails){
+        return (String.format("Thanks %s, We got your details with the following email: %s", userDetails.getName(), userDetails.getEmail()));
+    }
+
+}
