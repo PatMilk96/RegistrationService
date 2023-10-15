92f3ee1487246e2690cc97f4ba2818501d2c1f53
Patryk Milkiewicz <g00397265@atu.ie>
2023-10-15 17:09:45 +0100

    forgot to add endpoint for the Notification Service Client

--- src/main/java/ie/atu/registrationservice/NotificationServiceClient.java
+++ src/main/java/ie/atu/registrationservice/NotificationServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient
+@FeignClient(name = "notification-service", url = "http://localhost:8083")
 public interface NotificationServiceClient {
     @PostMapping("/notification")
     String notification(@RequestBody UserDetails userDetails);
