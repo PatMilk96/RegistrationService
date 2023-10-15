5f03eef41228d62b9f0d7f65b795f7b046c8a837
Patryk Milkiewicz <g00397265@atu.ie>
2023-10-15 17:03:54 +0100

    added code to return the message from the notification service

--- src/main/java/ie/atu/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -7,8 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
+    private final NotificationServiceClient notificationServiceClient;
+
+    @Autowired
+    public RegistrationController(NotificationServiceClient notificationServiceClient){
+        this.notificationServiceClient = notificationServiceClient;
+    }
+
     @PostMapping("/confirm")
     public String confDetails(@RequestBody UserDetails userDetails){
-        return (String.format("Thanks %s, We got your details with the following email: %s", userDetails.getName(), userDetails.getEmail()));
+        String notification = notificationServiceClient.notification(userDetails);
+        return (String.format("Thanks %s, We got your details with the following email: %s", userDetails.getName(), userDetails.getEmail() + "\n" + notification));
     }
 }
